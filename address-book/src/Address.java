3c961a41e0c7fc1657ad23c5208005fdb440adce
surbhi <surbhi@ainosoft.com>
2019-01-09 12:09:03 +0530

    new file surbhi

---
+++ address-book/src/Address.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Address {
+	private int id;
+	String name;
+	String street;
+	String city;
+	String country;
+	int zip;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+}
